8229e5840ee8da6e2fcd21312555914661c82253
Jiannine Torres <jiannine@agilebits.com>
2020-11-17 22:31:26 -0500

    Add Form styles

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,4 +1,42 @@
 import React, { useState } from "react";
+import styled from "styled-components";
+
+const FormStyles = styled.form`
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  border-radius: 6px;
+  width: 100%;
+  min-height: 60px;
+  background-color: var(--white);
+  padding: 0 var(--base-size);
+
+  label:not([for="checkbox"]) {
+    //hide label visually but keep them available to screen reader and other assistive technology
+    border: 0;
+    clip: rect(0 0 0 0);
+    height: 1px;
+    margin: -1px;
+    overflow: hidden;
+    padding: 0;
+    position: absolute;
+    width: 1px;
+  }
+
+  input[name="description"] {
+    border: 0;
+    flex-basis: 180px;
+    flex: 1 1 auto;
+    min-height: calc(var(--base-size) * 2);
+  }
+
+  input[name="location"] {
+    display: none;
+    @media screen and (min-width: 768px) {
+      display: block;
+    }
+  }
+`;
 
 const Form = ({ jobs }) => {
   const [description, setDescription] = useState("");
@@ -18,7 +56,7 @@ const Form = ({ jobs }) => {
   };
 
   return (
-    <form action="" method="get" onSubmit={onHandleSubmit}>
+    <FormStyles action="" method="get" onSubmit={onHandleSubmit}>
       <label htmlFor="description">
         Filter by title, companies, expertise...
       </label>
@@ -40,7 +78,7 @@ const Form = ({ jobs }) => {
       <label htmlFor="type">Full Time Only</label>
       <input type="checkbox" name="type" />
       <button type="submit">Search</button>
-    </form>
+    </FormStyles>
   );
 };
 
